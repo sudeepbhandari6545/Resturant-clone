afabb150c5cf4b1b38d3aa17aa894332b25aacdc
sudeepbhandari6545 <sudeepbhandari6545@gmail.com>
2021-07-28 09:39:54 +0545

    resturant

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Cards from './components/cards/Cards'
 const App = () => {
   return (
     <div>
-      <h1>sudeep bhandari</h1>
       <Cards />
     </div>
   )
--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,9 +1,31 @@
 import React from 'react'
+//
+import './Cards.css'
 
 const Cards = () => {
   return (
     <>
-      <div>this is first card components</div>
+      <div className="card_container">
+        <div className="card">
+          <div className="card_body">
+            <span className="card_number card_circle">1</span>
+            <span className="card_author">Breakfast</span>
+            <h2 className="card_title">Maggi</h2>
+            <span className="card_description">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt
+              impedit, sed magnam quisquam officia aliquid, exercitationem ullam
+              autem distinctio labore esse voluptas minima obcaecati
+              praesentium.
+            </span>
+            <div className="card_read">Read</div>
+          </div>
+          <img
+            src="https://www.seekpng.com/png/full/583-5832760_maggi-2-minutes-noodles-curry-maggi-noodles-saudi.png"
+            alt="image"
+          />
+          <span className="card_tag">Order Now</span>
+        </div>
+      </div>
     </>
   )
 }
