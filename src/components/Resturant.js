148585c9d7fedbe96d49f393e8a190b8476766ca
sudeepbhandari6545 <sudeepbhandari6545@gmail.com>
2021-07-29 12:09:10 +0545

    resturant

--- src/components/Resturant.js
+++ src/components/Resturant.js
@@ -2,11 +2,22 @@ import React, { useState } from 'react'
 import Menu from './cards/menuApi'
 import Cards from './cards/Cards'
 
+import './Navbar.css'
+
 const Resturant = () => {
   const [menuData, setMenuData] = useState(Menu)
 
   return (
     <>
+      <nav className="navbar">
+        <div className="btn_group">
+          <button className="btn_group_item">Breakfast</button>
+          <button className="btn_group_item">Lunch</button>
+          <button className="btn_group_item">Evening</button>
+          <button className="btn_group_item">Dinner</button>
+          <button className="btn_group_item">All</button>
+        </div>
+      </nav>
       <Cards menuData={menuData} />
     </>
   )
